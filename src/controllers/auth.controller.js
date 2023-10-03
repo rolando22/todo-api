70da8af0b1f5f572f24ef2f53be884cb93a37038
rolando22 <rolando2207@gmail.com>
2023-10-03 17:18:16 -0300

    [UPDATE] change credencials for login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 export class AuthController {
 
 	constructor({ authService }) {
-		this.authController = authService;
+		this.authService = authService;
 	}
 
 	login = (req, res) => {
-		const { username, password } = req.body;
-		const user = this.authController.login({ username, password });
+		const { email, password } = req.body;
+		const user = this.authService.login({ email, password });
 		if (!user) return res.status(401).json({ error: 'invalid user or password' });
 		res.json({ 
 			data: user, 
--- src/services/local-file-system/auth.service.js
+++ src/services/local-file-system/auth.service.js
@@ -4,15 +4,15 @@ import { users } from '../../mocks/users.js';
 
 export class AuthService {
 
-	static login({ username, password }) {
-		const user = users.find(user => user.username === username);
+	static login({ email, password }) {
+		const user = users.find(user => user.email === email);
 		const passwordCorrect = user === undefined ? false : user.password === password;
 		if (!(user && passwordCorrect)) return null;
 		const userForToken = {
 			id: user.id,
 			username: user.username,
 		};
-		const token = jwt.sign(userForToken, config.jwtSecret, { expiresIn: 60 * 60 * 24 });
+		const token = jwt.sign(userForToken, config.jwtSecret, { expiresIn: 1000 * 60 * 60 * 24 });
 		delete user.password;
 		return {
 			...user,
